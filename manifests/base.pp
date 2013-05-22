277c7a972c134e27d62308cd264a20cf267a29e6
Srikar Ayilavarapu <asrikar@gmail.com>
2013-05-22 15:58:07 -0300

    Fixing problems with rstudio support

--- manifests/base.pp
+++ manifests/base.pp
@@ -7,9 +7,10 @@ apt-package {["python-dev", "python-setuptools", "ipython-notebook"]: }
 apt-package {"python-pip": require => Package["python-dev"]}
 apt-package {["python-numpy", "python-scipy", "python-matplotlib"]: }
 apt-package {"libjpeg62": }
-apt-package {["r-base", "rstudio"]: }
+apt-package {["r-base", "wget"]: }
+class { "rstudio": }
 
-python-package {["nose", "nltk", "pycluster", "hcluster", "virtualenv", "python-hashes", "beautifulsoup4"]: }
+python-package {["nose", "nltk", "pycluster", "hcluster", "virtualenv", "python-hashes", "beautifulsoup4","pymorph", "mahotas"]: }
 
 define apt-package($package = $title) {
   package {$package:
@@ -23,5 +24,40 @@ define python-package($package = $title) {
     ensure   => present,
     provider => pip,
     require  => Package["python-pip"]
+  }
+}
+
+class rstudio {
+  $version = "0.97.551"
+  $home_dir = "/home/vagrant"
+  $package_loc = "${home_dir}/rstudio"
+  $package_name = "rstudio-${version}-amd64.deb"
+
+  file {$package_loc: 
+    ensure => directory,
+    owner  => vagrant,
+    group  => vagrant,
+    mode   => 0644
+  }
+
+  class {"rstudio-package": 
+    package_name => $package_name,
+    package_loc  => $package_loc
+  }
+
+  package {"rstudio":
+    provider => dpkg,
+    ensure   => installed,
+    source   => "${package_loc}/${package_name}",
+    require  => Class["rstudio-package"]
+  } 
+}
+
+class rstudio-package($package_name, $package_loc) {
+  exec {"wget http://download1.rstudio.org/${package_name}  -P ${package_loc} -o ${package_loc}/wget.log": 
+    user    => vagrant,
+    path    => ["/usr/local/sbin", "/bin", "/sbin", "/usr/bin", "/usr/sbin"],
+    creates => "${package_loc}/${package_name}",
+    require => [Package["wget"], File[$package_loc]]
   }
 }
